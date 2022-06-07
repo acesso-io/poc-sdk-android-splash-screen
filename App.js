766191e36be2d1f9e75f16f00fa6ce1fb869c8ed
Luiz Miyazaki <luiz.miyazaki@unico.io>
2022-06-07 15:00:05 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  NativeModules,
+  NativeEventEmitter,
+  ScrollView,
+} from 'react-native';
+import { logger } from 'react-native-logs';
+import SplashScreen from 'react-native-splash-screen';
+
+var log = logger.createLogger();
+
+const App = () => {
+  const [stateCamera, setStateCamera] = useState('Olá');
+
+  const { UnicoCheckModule } = NativeModules;
+  const eventEmitter = new NativeEventEmitter(UnicoCheckModule);
+
+  SplashScreen.hide()
+
+  useEffect(() => {
+    eventEmitter.addListener('onSuccess', e => {
+      setStateCamera(e.objResult);
+      log.info("sucesso-react-native");
+    });
+
+    eventEmitter.addListener('onError', e => {
+      setStateCamera(e.objResult);
+    });
+
+    return () => {
+      eventEmitter.removeAllListeners('onSuccess');
+      eventEmitter.removeAllListeners('onError');
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+      }}>
+      <View style={{ height: 200 }}>
+        <ScrollView>
+          <Text style={{ fontSize: 15, color: 'black' }}>{stateCamera}</Text>
+        </ScrollView>
+      </View>
+
+      <TouchableOpacity
+        activeOpacity={0.6}
+        onPress={() => UnicoCheckModule.callDefaultCamera()}
+        style={{
+          backgroundColor: 'red',
+          borderRadius: 5,
+          width: 150,
+          height: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text style={{ color: 'white', fontSize: 18 }}>Câmera Normal</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        activeOpacity={0.6}
+        onPress={() => UnicoCheckModule.callSmartCamera()}
+        style={{
+          backgroundColor: 'green',
+          borderRadius: 5,
+          width: 150,
+          height: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text style={{ color: 'white', fontSize: 18 }}>Câmera Smart</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        activeOpacity={0.6}
+        onPress={() => UnicoCheckModule.callLivenessCamera()}
+        style={{
+          backgroundColor: 'darkgray',
+          borderRadius: 5,
+          width: 150,
+          height: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text style={{ color: 'white', fontSize: 18 }}>Câmera Liveness</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        activeOpacity={0.6}
+        onPress={() => UnicoCheckModule.callDocumentCamera()}
+        style={{
+          backgroundColor: 'blue',
+          borderRadius: 5,
+          width: 150,
+          height: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text style={{ color: 'white', fontSize: 18 }}>Documentos</Text>
+      </TouchableOpacity>
+    </View >
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,26 @@
+# AcessoBio Suporte React Native
+
+Esta biblioteca visa oferecer uma PoC para suporte a implementação das bridges entre a SDK mobile nativa para camada em JS no React Native.
+
+Para executar o projeto primeiro realize as instalações das dependências com o seguinte comando:
+
+```
+npm install
+```
+
+Após a instalação, é necessário subir um servidor node na porta 8080:
+
+```
+npm start
+```
+
+Caso a porta esteja em uso por outra aplicação é necessário liberá-la. Para evitar erros.
+
+Por fim, rodamos o comando abaixo para subir o aplicativo no dispositivo android:
+
+```
+npm run android
+```
+
+---
+Observação: é necessário preencher o arquivo de credenciais (API Key) nos locais /android/app/src/main/assets/services.json (sem liveness) e /android/app/src/main/assets/services_ln.json (com liveness)
